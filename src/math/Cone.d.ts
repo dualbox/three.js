e97dcd30c9f35c6a4d799046050d8558b8478a93
Maxime Quiblier <maximequiblier@gmail.com>
2021-06-09 17:39:44 +0200

    Added Cone containsPoint

--- src/math/Cone.d.ts
+++ src/math/Cone.d.ts
@@ -3,21 +3,22 @@ import { Box3 } from "./Box3";
 
 export class Cone {
 
-	constructor( v?: Vector3, axis?: Vector3, theta?: number, inf?: number, sup?: number );
+    constructor( v?: Vector3, axis?: Vector3, theta?: number, inf?: number, sup?: number );
 
-	v: Vector3;
-	axis: Vector3;
-	theta: number;
-	inf: number;
-	sup: number;
+    v: Vector3;
+    axis: Vector3;
+    theta: number;
+    inf: number;
+    sup: number;
 
-	cosTheta: number;
+    cosTheta: number;
 
-	set( v?: Vector3, axis?: Vector3, theta?: number, inf?: number, sup?: number ): Cone;
-	clone(): Cone;
-	copy( cone: Cone );
-	empty(): boolean;
-	getBoundingBox(): Box3;
-	equals( cone: Cone ): boolean;
+    set( v?: Vector3, axis?: Vector3, theta?: number, inf?: number, sup?: number ): Cone;
+    clone(): Cone;
+    copy( cone: Cone );
+    empty(): boolean;
+    containsPoint( point: Vector3 ): boolean;
+    getBoundingBox(): Box3;
+    equals( cone: Cone ): boolean;
 
 }
--- src/math/Cone.js
+++ src/math/Cone.js
@@ -9,70 +9,87 @@ import { Vector3 } from "./Vector3";
  */
 export function Cone( v, axis, theta, inf, sup ) {
 
-	this.v = v || new Vector3();
-	this.axis = axis || new Vector3( 1, 0, 0 );
-	this.theta = theta || 0;
-	this.inf = inf || 0;
-	this.sup = sup || + Infinity;
+    this.v = v || new Vector3();
+    this.axis = axis || new Vector3( 1, 0, 0 );
+    this.theta = theta || 0;
+    this.inf = inf || 0;
+    this.sup = sup || + Infinity;
 
-	this.cosTheta = Math.cos( theta );
+    this.cosTheta = Math.cos( theta );
 
 }
 
 Object.assign( Cone.prototype, {
 
-	set: function ( v, axis, theta, inf, sup ) {
+    set: function ( v, axis, theta, inf, sup ) {
 
-		this.v.copy( v );
-		this.axis.copy( axis );
-		this.theta = theta;
-		this.inf = inf || 0;
-		this.sup = sup || + Infinity;
+        this.v.copy( v );
+        this.axis.copy( axis );
+        this.theta = theta;
+        this.inf = inf || 0;
+        this.sup = sup || + Infinity;
 
-		this.cosTheta = Math.cos( theta );
+        this.cosTheta = Math.cos( theta );
 
-		return this;
+        return this;
 
-	},
+    },
 
-	clone: function () {
+    clone: function () {
 
-		return new this.constructor().copy( this );
+        return new this.constructor().copy( this );
 
-	},
+    },
 
-	copy: function ( cone ) {
+    copy: function ( cone ) {
 
-		this.v.copy( cone.v );
-		this.axis.copy( cone.axis );
-		this.theta = cone.theta;
-		this.inf = cone.inf;
-		this.sup = cone.sup;
+        this.v.copy( cone.v );
+        this.axis.copy( cone.axis );
+        this.theta = cone.theta;
+        this.inf = cone.inf;
+        this.sup = cone.sup;
 
-		this.cosTheta = Math.cos( this.theta );
+        this.cosTheta = Math.cos( this.theta );
 
-		return this;
+        return this;
 
-	},
+    },
 
-	empty: function () {
+    empty: function () {
 
-		return ( this.theta <= 0 || this.inf >= this.sup );
+        return ( this.theta <= 0 || this.inf >= this.sup );
 
-	},
+    },
 
-	getBoundingBox: function ( target ) {
+    containsPoint: function () {
+        let tmp = new Vector3();
+        return function (point) {
+            tmp.subVectors(point, this.v);
+            let t = tmp.dot(this.axis);
+            if (t < this.inf || t > this.sup || Math.abs(t) < Number.MIN_VALUE) {
+                return false;
+            } else {
+                // radius at t
+                let r = Math.abs(t) * Math.tan(this.theta);
+                tmp.copy(this.axis).multiplyScalar(t).add(this.v);
+                tmp.sub(point);
+                return tmp.lengthSq() <= r * r;
+            }
+        };
+    }(),
 
-		throw "not implemented yet, todo";
+    getBoundingBox: function ( target ) {
 
-		return target;
+        throw "not implemented yet, todo";
 
-	},
+        return target;
 
-	equals: function ( cone ) {
+    },
 
-		return cone.v.equals( this.v ) && cone.axis.equals( this.axis ) && cone.theta === this.theta && cone.inf === this.inf && cone.sup === this.sup;
+    equals: function ( cone ) {
 
-	}
+        return cone.v.equals( this.v ) && cone.axis.equals( this.axis ) && cone.theta === this.theta && cone.inf === this.inf && cone.sup === this.sup;
+
+    }
 
 } );
